0500652ac7eb92968d1e64d6973ab41b9626c5b5
Baptiste Denis <67144261+Ne-d@users.noreply.github.com>
2024-07-17 19:01:27 +0200

    Add documentation for the Metric type.

--- src/Client/Metrics/Metric.h
+++ src/Client/Metrics/Metric.h
@@ -6,6 +6,13 @@
 #include <string>
 
 namespace Cesame {
+/**
+ * A variant representing a metric (such as CPU temperature, disk usage, etc.).
+ * It can be either a Quantity or a string.
+ *
+ * @tparam Unit The unit of the metric, if it's a quantity.
+ * @tparam Ref The underlying type representing the quantity (int, double, etc.).
+ */
 template <typename Unit, typename Ref>
 using Metric = std::variant<au::Quantity<Unit, Ref>, std::string>;
 }
