f6b43e6072756d8acb30de656b976f051815714e
Baptiste Denis <67144261+Ne-d@users.noreply.github.com>
2024-07-18 02:10:43 +0200

    Add the TimeManager class.

---
+++ src/Client/TimeManager.h
@@ -0,0 +1,38 @@
+#ifndef TIMEMANAGER_H
+#define TIMEMANAGER_H
+
+#include <QTimer>
+
+namespace Cesame {
+/**
+ * A wrapper for a QTimer.
+ * All widgets that need to update at once can connect to the same instance of this.
+ */
+class TimeManager {
+private: // Fields
+    QTimer timer;
+
+public: //Methods
+    explicit TimeManager(std::chrono::milliseconds interval);
+
+    QTimer& getTimer();
+
+    /**
+     * Wrapper for the connect method of the timer.
+     */
+    QMetaObject::Connection connect(const QObject* sender,
+                                    const char* signal, const char* method,
+                                    Qt::ConnectionType type = Qt::AutoConnection) const;
+
+    void setInterval(std::chrono::milliseconds interval);
+
+    static std::chrono::time_point<std::chrono::steady_clock> getCurrentTimePoint();
+};
+
+/**
+ * A global instance of TimeManager which all "normal" widgets (that update every second) will connect to.
+ */
+static TimeManager globalTimeManager(std::chrono::milliseconds(1000));
+}
+
+#endif //TIMEMANAGER_H
