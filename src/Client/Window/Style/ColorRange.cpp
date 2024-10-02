0d8a7da0b3104129acd0b8e39e6492ff35a9c765
Baptiste Denis <67144261+Ne-d@users.noreply.github.com>
2024-10-02 12:00:13 +0200

    Add ColorRange and ColorRangeList

---
+++ src/Client/Window/Style/ColorRange.cpp
@@ -0,0 +1,61 @@
+#include "ColorRange.h"
+
+#include <list>
+
+namespace Cesame {
+// ColorRange Class
+
+ColorRange::ColorRange(const double lowerBound, const double upperBound, const QColor color) :
+    lowerBound(lowerBound),
+    upperBound(upperBound),
+    color(color) {}
+
+double ColorRange::getLowerBound() const {
+    return lowerBound;
+}
+
+void ColorRange::setLowerBound(const double lowerBound) {
+    this->lowerBound = lowerBound;
+}
+
+double ColorRange::getUpperBound() const {
+    return upperBound;
+}
+
+void ColorRange::setUpperBound(const double upperBound) {
+    this->upperBound = upperBound;
+}
+
+QColor ColorRange::getColor() const {
+    return color;
+}
+
+void ColorRange::setColor(const QColor& color) {
+    this->color = color;
+}
+
+
+// ColorRangeList Class
+
+ColorRangeList::ColorRangeList(QList<ColorRange> list) : colorRanges(std::move(list)) {}
+
+QColor ColorRangeList::getColor(const double value) const {
+    const ColorRange& firstRange = colorRanges.constFirst();
+    const ColorRange& lastRange = colorRanges.constLast();
+
+    if (value < firstRange.getLowerBound())
+        return colorRanges.constFirst().getColor();
+
+    if (value > lastRange.getUpperBound())
+        return colorRanges.constLast().getColor();
+
+    for (const ColorRange& range : colorRanges) {
+        // If the value is within the bounds of the ColorRange
+        if (range.getLowerBound() < value && range.getUpperBound() > value) {
+            return range.getColor();
+        }
+    }
+
+    return firstRange.getColor();
+}
+} // Cesame
---
+++ src/Client/Window/Style/ColorRange.h
@@ -0,0 +1,40 @@
+#ifndef COLORRANGE_H
+#define COLORRANGE_H
+
+#include <QColor>
+
+namespace Cesame {
+/**
+ * Represents a relation between a range of values and a color.
+ * This can be used to set "alarm colors" for values that are too high, for example.
+ */
+class ColorRange {
+public:
+    ColorRange(double lowerBound, double upperBound, QColor color);
+
+    // Accessors
+    [[nodiscard]] double getLowerBound() const;
+    void setLowerBound(double lowerBound);
+    [[nodiscard]] double getUpperBound() const;
+    void setUpperBound(double upperBound);
+    [[nodiscard]] QColor getColor() const;
+    void setColor(const QColor& color);
+
+private:
+    double lowerBound;
+    double upperBound;
+    QColor color;
+};
+
+class ColorRangeList {
+public:
+    explicit ColorRangeList(QList<ColorRange> list);
+
+    [[nodiscard]] QColor getColor(double value) const;
+
+private:
+    QList<ColorRange> colorRanges;
+};
+} // Cesame
+
+#endif //COLORRANGE_H
